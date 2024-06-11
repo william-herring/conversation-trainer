58354bb35cbb2a79c13d8b929ce82508743916e3
William Herring <73984595+william-herring@users.noreply.github.com>
2024-06-12 08:19:12 +1000

    add basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/conversation-trainer",
+  basePath: "/conversation-trainer",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
