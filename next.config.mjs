4cbebfb44249dc57adc8f25909e29387a2976676
William Herring <73984595+william-herring@users.noreply.github.com>
2024-06-15 13:15:41 +1000

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/conversation-trainer",
-  output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
 
